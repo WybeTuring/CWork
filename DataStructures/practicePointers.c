4c07b7eb6db2ac3bc855fb3cc5046a6a4124a233
WybeTuring <Karllemfon@gmail.com>
2019-01-07 18:21:47 +0000

    COmments

--- DataStructures/practicePointers.c
+++ DataStructures/practicePointers.c
@@ -7,6 +7,7 @@ Intent: This is simply practice for the study I am doing on pointers
 #include <stdio.h>
 #include <stdlib.h>
 
+//Decomposes the double number into an interger part and a fractional int_part
 void decompose(double x, long *int_part, double *frac_part){
   *int_part = (long)x;
   *frac_part = x - *int_part;
