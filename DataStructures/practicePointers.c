d7fe05a7598560866d6da0adb49b92394faeeb1b
WybeTuring <Karllemfon@gmail.com>
2019-01-07 18:19:47 +0000

    Decompose doubles This function decomposes doubles into interge and remainding double parts. This uses pointers as practice for pointers.

---
+++ DataStructures/practicePointers.c
@@ -0,0 +1,23 @@
+/*
+Author: Lemfon Karl Ndze'dzenyuy
+Email: karllemfon@gmail.com
+GitHub: WybeTuring
+Intent: This is simply practice for the study I am doing on pointers
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+void decompose(double x, long *int_part, double *frac_part){
+  *int_part = (long)x;
+  *frac_part = x - *int_part;
+  printf("%ld\n",*int_part);
+  printf("%d\n",*frac_part);
+}
+
+
+int main(){
+  long i=0;
+  double d=0.9898;
+  decompose(3.5476352,&i,&d);
+  return 0;
+}
